005498d3f8390f4b9d1101e14a3248158fae3b6b
liyanippon <ll>
2017-04-26 17:32:25 +0800

    [add]添加BottomTabDemo到android开发资料

---
+++ android开发资料/dataStu/BottomTabDemo/src/com/xiaowu/bottomtab/demo/MeFragment.java
@@ -0,0 +1,28 @@
+package com.xiaowu.bottomtab.demo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * “我的”碎片页面
+ * @author wwj_748
+ *
+ */
+public class MeFragment extends Fragment {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.main_tab3_fragment, container,
+				false);
+		return view;
+	}
+
+}
