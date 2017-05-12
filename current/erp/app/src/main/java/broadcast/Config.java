5e05c774acab6096f4a64f56de7fdcb657470ced
liyanippon <ll>
2017-05-12 15:26:18 +0800

    [del]去除无用的文件

--- current/erp/app/src/main/java/broadcast/Config.java
+++
@@ -1,9 +0,0 @@
-package broadcast;
-
-/**
- * Created by liyanippon on 17/04/03.
- */
-
-public class Config {
-    public static String BC_ONE = "com.example.testbroadcast.bcone";
-}
--- current/erp/app/src/main/java/portface/LazyLoadFace.java
+++
@@ -1,9 +0,0 @@
-package portface;
-
-/**
- * Created by admin on 2017/3/8.
- */
-
-public interface LazyLoadFace {
-    void AdapterRefresh(String type);//刷新适配器
-}
