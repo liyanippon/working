5bc3c65a651cfc0d732ef55024eb672c80e3166b
liyanippon <liyanippon@163.com>
2018-01-19 14:28:01 +0800

    [add]搭建我的博客页面展示

--- data/work/javaweb/workspace/myblog/src/main/java/com/ifeng/controller/MyBlogController.java
+++ data/work/javaweb/workspace/myblog/src/main/java/com/ifeng/controller/MyBlogController.java
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MyBlogController {
 	private static Logger logger = Logger.getLogger(DocumentController.class);
 	
-	@RequestMapping("/home") /*家 首页*/
-	public String toHome(HttpServletRequest request,ModelMap map) throws Exception{
+	@RequestMapping("/index") /*index 首页*/
+	public String Index(HttpServletRequest request,ModelMap map) throws Exception{
 		request.setCharacterEncoding("UTF-8");//解决中文乱码问题
 		
-		return "myblog/index";
+		return "index";
 	}
 }
